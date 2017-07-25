fd4e6a53de9552fc715280bc7ffa3fe0c835a9a7
Louis Delbosc <louisdelbosc@gmail.com>
2017-07-25 22:31:59 +0200

    Improve lisibility

--- secret-handshake/secret_handshake.exs
+++ secret-handshake/secret_handshake.exs
@@ -1,4 +1,14 @@
+import Bitwise
+
 defmodule SecretHandshake do
+
+  @commands %{
+    0b1 => "wink",
+    0b10 => "double blink",
+    0b100 => "close your eyes",
+    0b1000 => "jump"
+  }
+
   @doc """
   Determine the actions of a secret handshake based on the binary
   representation of the given `code`.
@@ -13,8 +23,19 @@ defmodule SecretHandshake do
 
   10000 = Reverse the order of the operations in the secret handshake
   """
+  def reverse(list, code) do
+    if (code &&& 0b10000) != 0 do
+      Enum.reverse list
+    else
+      list
+    end
+  end
+
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
+    in_list? = fn(x) -> (x &&& code) != 0 end
+    (for {a, b} <- @commands, in_list?.(a), do: b)
+    |> reverse(code)
   end
 end
 
