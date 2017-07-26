298d174d02e8ea43ca2f823e8688bb88f2063876
Louis Delbosc <louisdelbosc@gmail.com>
2017-07-26 23:19:42 +0200

    Improve solution

--- rotational-cipher/rotational_cipher.exs
+++ rotational-cipher/rotational_cipher.exs
@@ -7,24 +7,19 @@ defmodule RotationalCipher do
   "Nggnpx ng qnja"
   """
 
-  def shift_letter(binary_letter, shift) do
+  def shift_letter(char, shift) do
     cond do
-      (binary_letter >= ?a) and (binary_letter <= ?z) ->
-        rem(binary_letter - ?a + shift, 26) + ?a
-
-      (binary_letter >= ?A) and (binary_letter <= ?Z) ->
-        rem(binary_letter - ?A + shift, 26) + ?A
-
-      true ->
-        binary_letter
+      char in ?a..?z -> rem(char - ?a + shift, 26) + ?a
+      char in ?A..?Z -> rem(char - ?A + shift, 26) + ?A
+      true -> char
     end
   end
 
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
-    partial_shift = fn(x) -> shift_letter(x, shift) end
-    to_charlist(text)
-    |> Enum.map(partial_shift)
+    text
+    |> to_charlist
+    |> Enum.map(&(shift_letter(&1, shift)))
     |> to_string
   end
 end
