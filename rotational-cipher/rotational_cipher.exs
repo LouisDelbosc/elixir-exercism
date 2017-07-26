dd66ac15920b586993836d3d80776dd843b69747
Louis Delbosc <louisdelbosc@gmail.com>
2017-07-26 23:11:27 +0200

    Solve ceasar cipher in elixir

--- rotational-cipher/rotational_cipher.exs
+++ rotational-cipher/rotational_cipher.exs
@@ -6,8 +6,26 @@ defmodule RotationalCipher do
   iex> RotationalCipher.rotate("Attack at dawn", 13)
   "Nggnpx ng qnja"
   """
+
+  def shift_letter(binary_letter, shift) do
+    cond do
+      (binary_letter >= ?a) and (binary_letter <= ?z) ->
+        rem(binary_letter - ?a + shift, 26) + ?a
+
+      (binary_letter >= ?A) and (binary_letter <= ?Z) ->
+        rem(binary_letter - ?A + shift, 26) + ?A
+
+      true ->
+        binary_letter
+    end
+  end
+
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
+    partial_shift = fn(x) -> shift_letter(x, shift) end
+    to_charlist(text)
+    |> Enum.map(partial_shift)
+    |> to_string
   end
 end
 
