b184687fa0bebca5786a6f50b5ea272ef8ce0498
Louis Delbosc <louisdelbosc@gmail.com>
2017-08-01 23:19:13 +0200

    Bob clean with help of others

---
+++ bob/bob.exs
@@ -0,0 +1,11 @@
+defmodule Bob do
+  def hey(input) do
+    cond do
+      String.ends_with?(input, "?") -> "Sure."
+      String.trim(input) == "" -> "Fine. Be that way!"
+      String.upcase(input) == input and String.downcase(input) != input
+        -> "Whoa, chill out!"
+      true -> "Whatever."
+    end
+  end
+end
