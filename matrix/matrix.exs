07c2e2e37e24f036829055ae31a5fcc24e7b875a
Louis Delbosc <louisdelbosc@gmail.com>
2017-08-20 23:23:10 +0200

    Matrix done

---
+++ matrix/matrix.exs
@@ -0,0 +1,63 @@
+defmodule Matrix do
+  defstruct matrix: nil
+
+  @doc """
+  Convert an `input` string, with rows separated by newlines and values
+  separated by single spaces, into a `Matrix` struct.
+  """
+  @spec from_string(input :: String.t()) :: %Matrix{}
+  def from_string(input) do
+    for arr <- String.split(input, "\n") do
+      arr
+      |> (&(String.split(&1, " "))).()
+      |> Enum.map(&String.to_integer/1)
+    end
+  end
+
+  @doc """
+  Write the `matrix` out as a string, with rows separated by newlines and
+  values separated by single spaces.
+  """
+  @spec to_string(matrix :: %Matrix{}) :: String.t()
+  def to_string(matrix) do
+    matrix
+    |> Enum.map(&(Enum.join(&1, " ")))
+    |> Enum.join("\n")
+  end
+
+  @doc """
+  Given a `matrix`, return its rows as a list of lists of integers.
+  """
+  @spec rows(matrix :: %Matrix{}) :: list(list(integer))
+  def rows(matrix) do
+    matrix
+  end
+
+  @doc """
+  Given a `matrix` and `index`, return the row at `index`.
+  """
+  @spec row(matrix :: %Matrix{}, index :: integer) :: list(integer)
+  def row([row_head | row_tail], 0), do: row_head
+  def row([row_head | row_tail], index) do
+    row(row_tail, index - 1)
+  end
+
+  @doc """
+  Given a `matrix`, return its columns as a list of lists of integers.
+  """
+  @spec columns(matrix :: %Matrix{}) :: list(list(integer))
+  def columns(matrix) do
+    depth = matrix |> (&List.first/1).() |> length
+    for index <- 0..depth-1, do: column(matrix, index)
+  end
+
+  @doc """
+  Given a `matrix` and `index`, return the column at `index`.
+  """
+  @spec column(matrix :: %Matrix{}, index :: integer) :: list(integer)
+  def column(matrix, index) do
+    matrix
+    |> Enum.map(&(Enum.at(&1, index)))
+  end
+end
+
