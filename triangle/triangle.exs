6bafb811498c37f5dd12ee6b4ec637b4e5ea933a
Louis Delbosc <louisdelbosc@gmail.com>
2017-08-29 22:19:49 +0200

    Clean triangle

--- triangle/triangle.exs
+++ triangle/triangle.exs
@@ -4,15 +4,25 @@ defmodule Triangle do
   @doc """
   Return the kind of triangle of a triangle with 'a', 'b' and 'c' as lengths.
   """
-  @spec kind(number, number, number) :: { :ok, kind } | { :error, String.t }
-  def kind(a, b, c) when a <= 0 or b <= 0 or c <= 0, do: { :error, "all side lengths must be positive" }
+  @spec kind(number, number, number) :: {:ok, kind} | {:error, String.t}
+  def kind(a, b, c)
+  when a <= 0
+  when b <= 0
+  when c <= 0, do: {:error, "all side lengths must be positive"}
+
+  def kind(a, b, c)
+  when a + b <= c
+  when a + c <= b
+  when b + c <= a, do: {:error, "side lengths violate triangle inequality"}
+
   def kind(a, a, a), do: {:ok, :equilateral}
-  def kind(a, b, c) do
-    [biggest | rest] = Enum.sort([a, b, c], &(&1 >= &2))
-    cond do
-      biggest >= Enum.sum(rest) -> { :error, "side lengths violate triangle inequality" }
-      a == b or b == c or a == c -> {:ok, :isosceles}
-      true -> {:ok, :scalene}
-    end
+
+  def kind(a, b, c)
+  when a == b
+  when b == c
+  when a == c, do: {:ok, :isosceles}
+
+  def kind(_, _, _) do
+    {:ok, :scalene}
   end
 end
