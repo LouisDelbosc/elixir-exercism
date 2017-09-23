d0a32214cb479a8c40307542d66e0023da9a093c
Louis Delbosc <louisdelbosc@gmail.com>
2017-09-23 18:01:05 +0200

    Improve pascal triangle

--- pascals-triangle/pascals_triangle.exs
+++ pascals-triangle/pascals_triangle.exs
@@ -7,13 +7,14 @@ defmodule PascalsTriangle do
   def rows(1), do: [[1]]
   def rows(num) do
     1..num-1
-    |> Enum.reduce([[1]], fn _, acc = [h|t] -> [ next_row(h) | acc ] end)
+    |> Enum.reduce([[1]], fn _, acc = [h|_] -> [ next_row(h) | acc ] end)
     |> Enum.reverse
   end
 
-  def next_row([], acc, previous_value), do: [ previous_value | acc ]
-  def next_row([h|t], acc, previous_value) do
-    next_row(t, [ previous_value + h | acc ], h)
+  def next_row(previous_row) do
+    previous_row
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.map(&Enum.sum/1)
+    |> (&([1] ++ &1 ++ [1])).()
   end
-  def next_row(previous_row), do: next_row(previous_row, [], 0)
 end
