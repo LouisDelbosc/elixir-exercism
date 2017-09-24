3e6c045994051cadc1e54b8ecbd0624b23febf78
Louis Delbosc <louisdelbosc@gmail.com>
2017-09-24 18:48:33 +0200

    saddle point done

---
+++ saddle-points/saddle_points.exs
@@ -0,0 +1,37 @@
+defmodule SaddlePoints do
+  @doc """
+  Parses a string representation of a matrix
+  to a list of rows
+  """
+  @spec rows(String.t()) :: [[integer]]
+  def rows(str) do
+    for arr <- String.split(str, "\n") do
+      arr
+      |> (&(String.split(&1, " "))).()
+      |> Enum.map(&String.to_integer/1)
+    end
+  end
+
+  @doc """
+  Parses a string representation of a matrix
+  to a list of columns
+  """
+  @spec columns(String.t()) :: [[integer]]
+  def columns(str) do
+    str
+    |> rows
+    |> List.zip
+    |> Enum.map(&Tuple.to_list/1)
+  end
+
+  @doc """
+  Calculates all the saddle points from a string
+  representation of a matrix
+  """
+  @spec saddle_points(String.t()) :: [{integer, integer}]
+  def saddle_points(str) do
+    maxs = str |> rows |> Enum.map(&Enum.max/1) |> Enum.with_index
+    mins = str |> columns |> Enum.map(&Enum.min/1) |> Enum.with_index
+    for {row, i} <- maxs, {column, j} <- mins, row == column, do: {i, j}
+  end
+end
